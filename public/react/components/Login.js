56fa27c08694e9fa01160376d319c66ea3a93bc1
Yohonna <yoyohakeem@gmail.com>
2023-03-16 12:33:14 -0500

    added comments

--- public/react/components/Login.js
+++ public/react/components/Login.js
@@ -12,59 +12,9 @@ export const Login = ({ setIsLoggedIn, setIsHome ,setSingleViewUser }) => {
   const [password, setPassword] = useState("");
 
   async function handleLogin(ev) {
- 
-//login a user and check that their info matches database
-// router.post("/login", authUser, async (req, res, next) => {
-//   try {
-//     let { user_name, password } = req.body;
-//     let loginUser = await User.findOne({
-//       where: { user_name },
-//     });
-
-//     // Authenticate the loginUser
-//     let isMatching = await bcrypt.compare(password, loginUser.password);
-//     if (isMatching) {
-//       // If True, the loginUser successfully logged in.
-//       //  Deconstructing the User Object by its properties/fields.
-//       const { id, user_name } = loginUser;
-//       let payload = { id, user_name };
-
-//       // Generate a token with payload and a secret
-//       const token = jwt.sign(payload, ACCESS_TOKEN_SECRET);
-//       res.send({ message: "Successful Login", token });
-//     } else {
-//       res.send("Please enter the correct password and try again.");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     next(error);
-//   }
-// });
-
-
-//  checks if someone is a chef
-//write logic to check if a person is logged in or not. 
-// router.post("/users", authUser, async (req, res, next) => {
-//   const { isUser } = req.body;
-//   if (!isChef) {
-//     res.send({ message: "Not authorized, please login or register" });
-//   }
-//   try {
-//     const user = await User.create(req.body);
-//     res.send(user);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
- 
-
-
- 
- 
- 
-
-
+ //add logic here
+ //see users.js in routes for details
+// needs to route to UserProfile component once someone clicks the login button.
     
   }
   function refreshPage() {
